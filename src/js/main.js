a76c830396d82a647d1e281b75330839528afece
Jerry Vargas <vargasman11@gmail.com>
2022-01-23 23:22:11 -0600

    add

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,17 @@
-let url = 'https://rickandmortyapi.com/api/character';
+if ($("#name").val() === '') {
+    console.log("no change")
+    let url = 'https://rickandmortyapi.com/api/character';
+    $.get(`${url}`, function (res, status) {
+            if (status === 'success') {
+                let characters = res.results;
+                characters.forEach(el => {
+                    let imgUrl = el.image;
+                    let name = el.name;
 
-let errMessage = "There was something wrong with the request";
-let removeCharacter = function(id){
-    $("#character-"+id).remove();
-}
-
-
-$.get(`${url}`, function (res, status, xhr) {
-        if (status == 'success') {
-            console.log(res)
-            let characters = res.results;
-            // characters.forEach(el => console.log(el))
-            characters.forEach(el => {
-                let imgUrl = el.image;
-                let name = el.name;
-
-                $('.article').append(
-                    $(`<div class="card" id="character-${el.id}">
+                    $('.article').append(
+                        $(`<div class="card" id="character-${el.id}">
                         <div class="card__img-container">
-                            <img class="card__img" src=${imgUrl}>
+                            <img class="card__img" src=${imgUrl} alt="img">
                         </div>
                         <div class="card__meta">
                             <div class="card__meta__name">${name}</div>
@@ -26,19 +19,57 @@ $.get(`${url}`, function (res, status, xhr) {
                         </div>
                      </div>
                     `)
-                );
+                    );
 
-                $(`#remove-ch-${el.id}`).click(function (){
-                    $(`#character-${el.id}`).remove();
+                    $(`#remove-ch-${el.id}`).click(function () {
+                        $(`#character-${el.id}`).remove();
+                    })
                 })
-            })
-        } else {
-            console.log(errMessage)
+            }
         }
-    }
-);
+    );
+}
+
+$("#name").change(function () {
+    let searchText = $('#name').val();
+    console.log("there is a change")
+    $(".article").remove();
+    $(".article-container").append(`<div class="article"></div>`)
+    let url = `https://rickandmortyapi.com/api/character/?name=` + searchText;
+    $.get(`${url}`, function (res, status) {
+            if (status === 'success') {
+                let characters = res.results;
+                characters.forEach(el => {
+                    let imgUrl = el.image;
+                    let name = el.name;
+
+                    $('.article').append(
+                        $(`<div class="card" id="character-${el.id}">
+                        <div class="card__img-container">
+                            <img class="card__img" src=${imgUrl} alt="img">
+                        </div>
+                        <div class="card__meta">
+                            <div class="card__meta__name">${name}</div>
+                            <div class="card__meta__delete btn" id="remove-ch-${el.id}">Delete</div>
+                        </div>
+                     </div>
+                    `)
+                    );
+
+                    $(`#remove-ch-${el.id}`).click(function () {
+                        $(`#character-${el.id}`).remove();
+                    })
+                })
+            }
+        }
+    );
+})
 
 
+
+let removeCharacter = function (id) {
+    $("#character-" + id).remove();
+}
 
 
 
